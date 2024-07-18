0c7666540bed690ed7c8675e66038a5a17e164a0
dkks112313 <94787767+dkks112313@users.noreply.github.com>
2024-07-18 18:04:49 +0300

    fix

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import sys
-from PyQt6.QtGui import QIcon
 from src import app
 from PyQt6.QtWidgets import QApplication
 
@@ -7,7 +6,6 @@ from PyQt6.QtWidgets import QApplication
 def main():
     apps = QApplication(sys.argv)
     window = app.MainWindow()
-    window.setWindowIcon(QIcon("icon.ico"))
     window.setFixedSize(700, 700)
     window.show()
     sys.exit(apps.exec())
