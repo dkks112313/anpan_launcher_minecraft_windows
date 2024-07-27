7a317dd0ad85d144e8b4149059fea0c5e0ecd7d0
ovcha <ovcharenkoden05@gmail.com>
2024-07-27 23:35:12 +0300

    big win

---
+++ src/folder.py
@@ -0,0 +1,18 @@
+import os
+from src import env
+import shutil
+
+
+def removing_folder_resources():
+    folder_from = rf'{os.path.join(os.getcwd(), 'resources')}'
+    folder_to = rf'{os.path.join(env.settings['minecraft_directory'], 'resources')}'
+
+    print(folder_from)
+    print(folder_to)
+
+    for f in os.listdir(folder_from):
+        if os.path.isfile(os.path.join(folder_from, f)):
+            shutil.copy(os.path.join(folder_from, f), os.path.join(folder_to, f))
+        if os.path.isdir(os.path.join(folder_from, f)):
+            os.system(f'rd /S /Q {folder_to}\\{f}')
+            shutil.copytree(os.path.join(folder_from, f), os.path.join(folder_to, f))
