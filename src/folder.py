037e184a65b2d179c2c4c6da5cddeff7848c79e9
ovcha <ovcharenkoden05@gmail.com>
2024-08-01 12:23:12 +0300

    big win

--- src/folder.py
+++ src/folder.py
@@ -7,9 +7,10 @@ def moving_folder_resources():
     folder_from = rf'{os.path.join(os.getcwd(), 'resources')}'
     folder_to = rf'{os.path.join(env.settings['minecraft_directory'], 'resources')}'
 
-    for f in os.listdir(folder_from):
-        if os.path.isfile(os.path.join(folder_from, f)):
-            shutil.copy(os.path.join(folder_from, f), os.path.join(folder_to, f))
-        if os.path.isdir(os.path.join(folder_from, f)):
-            os.system(f'rd /S /Q {folder_to}\\{f}')
-            shutil.copytree(os.path.join(folder_from, f), os.path.join(folder_to, f))
+    if not os.path.exists(folder_from):
+        return
+
+    if os.path.exists(folder_to):
+        shutil.rmtree(folder_to)
+
+    shutil.copytree(folder_from, folder_to)
