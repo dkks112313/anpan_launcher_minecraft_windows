c1aba230cd42b84447a7065effb5d68067bfd214
Denchi2414 <94787767+dkks112313@users.noreply.github.com>
2024-08-06 14:16:35 +0300

    Add files via upload

---
+++ run/update.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"os/exec"
+	"syscall"
+
+	"golang.org/x/sys/windows"
+)
+
+func main() {
+	// Подготовка команды для запуска
+	cmd := exec.Command("./upk.exe")
+
+	// Настройка атрибутов процесса для скрытия окна
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		CreationFlags: windows.CREATE_NO_WINDOW,
+	}
+
+	// Запуск команды
+	err := cmd.Start()
+	if err != nil {
+		panic(err)
+	}
+}
